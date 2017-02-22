e5cfbc6816a73ff304f03259023e2cf4857938f3
Webmaster10 <craigm1997@gmail.com>
2017-02-22 22:42:07 +0000

    Improved player jump, made camera follow player, added coins

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -3,19 +3,23 @@ using System.Collections;
 
 public class Player : MonoBehaviour {
 
-	// variables taken from CharacterController.Move example script
-	// https://docs.unity3d.com/ScriptReference/CharacterController.Move.html
+    // variables taken from CharacterController.Move example script
+    // https://docs.unity3d.com/ScriptReference/CharacterController.Move.html
+
+    public AudioSource jumpSound;
 	public float speed = 6.0F;
-	public float jumpSpeed = 8.0F;
-	public float maxJumpHeight = 2.0f;
+    public float airSpeed = 2.0F;
+    public float jumpSpeed = 8.0F;
+    public float maxJumpTime = 0.3f; // max time the jump button can be held in seconds
 	public float gravity = 20.0F;
 	private Vector3 moveDirection = Vector3.zero;
 
-	private float jumpStartHeight = 0.0f;
-	private bool jumping = false;
+    private float jumpTime = 0.0f;
+    private bool falling = false;
 	private Vector3 lastPosition;
 
 	public int Lives = 3; // number of lives the player hs
+    public int coins = 0;
 
 
 	Vector3 start_position; // start position of the player
@@ -45,39 +49,37 @@ public class Player : MonoBehaviour {
 
 		lastPosition = transform.position;
 
-		// check to see if the player is on the ground
-		if (controller.isGrounded) 
-		{
-			// set the movement direction based on user input and the desired speed
-			moveDirection = new Vector3(Input.GetAxis("Horizontal"), 0, 0);
-			moveDirection = transform.TransformDirection(moveDirection);
-			moveDirection *= speed;
-			// reset jump height
-			jumping = false;
-			jumpStartHeight = transform.position.y;
-		}
+		// set the movement direction based on user input and the desired speed
+		
+        if (controller.isGrounded)
+        {
+            moveDirection = new Vector3(Input.GetAxis("Horizontal"), 0, 0);
+            moveDirection = transform.TransformDirection(moveDirection);
+            moveDirection *= speed;
+            // reset jump height
+            falling = false;
+            jumpTime = 0.0f;
+        }
 
-<<<<<<< HEAD
 		// check to see if the player should jump
-		if (Input.GetButton("Jump") && ((transform.position.y - jumpStartHeight) < maxJumpHeight) && (lastPosition.y <= transform.position.y))
+		if (Input.GetButton("Jump") && falling == false && jumpTime <= maxJumpTime)
 		{
-			if (jumping == false)
-			{
-				jumpStartHeight = transform.position.y;
-				jumping = true;
-			}
+            if (jumpTime == 0.0f)
+                jumpSound.Play();
+            jumpTime += Time.deltaTime;
 			moveDirection.y = jumpSpeed;
-=======
-            // check to see if the player should jump
-            if (Input.GetButton("Jump"))
-                jump();
->>>>>>> refs/remotes/MrSlagovich/master
 		}
+        else
+        {
+            jumpTime -= Time.deltaTime;
+            falling = true;
+        }
 
 		// apply gravity to movement direction
 		moveDirection.y -= gravity * Time.deltaTime;
 
 		// make the call to move the character controller
 		controller.Move(moveDirection * Time.deltaTime);
+
 	}
 }
